79d18cf2c4ae021f218e4933fe7d03c8de260573
Xem0n <biedukiewicz87@gmail.com>
2023-03-09 10:32:37 +0100

    add more status

--- confirm.php
+++ confirm.php
@@ -9,15 +9,67 @@ if (!$_SESSION["user"] ?? null || $_SESSION["user"]->role != "admin") {
     header("Location: login.php");
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pizza_id = intval($_POST["transaction_id"]) ?? 0;
+$user = $_SESSION["user"];
 
-    if ($pizza_id > 0) {
-        pizza_receive($pizza_id);
+$PREPARED = 0;
+$DELIVERED = 1;
+$RECEIVED = 2;
+
+$check = array();
+$check[$PREPARED] = function($transaction) {
+    global $user;
+    return ($transaction["prepared_time"] == NULL && $transaction["delivery_time"] == NULL && $user->role == "admin");
+};
+$check[$DELIVERED] = function($transaction) {
+    global $user;
+    return ($transaction["prepared_time"] != NULL && $transaction["delivery_time"] == NULL && $user->role == "deliverer");
+};
+$check[$RECEIVED] = function($transaction) {
+    global $user;
+    return ($transaction["prepared_time"] != NULL && $transaction["delivery_time"] != NULL && $user->role == "deliverer");
+};
+
+$actions = array();
+$actions[$PREPARED] = function($transaction) {
+    pizza_prepared($transaction);
+};
+$actions[$DELIVERED] = function($transaction) {
+    pizza_deliver($transaction);
+
+};
+$actions[$RECEIVED] = function($transaction) {
+    pizza_receive($transaction);
+};
+
+function display_form($data, $status, $text) {
+    ?>
+
+    <form action="?" method="post">
+        <input type="hidden" name="transaction_id" id="transaction_id" value="<?= $data["id"] ?>">
+        <input type="hidden" name="status" id="status" value="<?= $status ?>">
+        <button class="button is-primary"><?= $text ?></button>
+    </form>
+
+    <?php
+};
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $transaction = intval($_POST["transaction_id"] ?? 0) ;
+    $status = intval($_POST["status"] ?? -1);
+
+    if ($transaction > 0 && $status >= 0) {
+        $transaction_data = $mysqli->prepare("SELECT prepared_time, delivery_time FROM transactions WHERE id=?");
+        $transaction_data->bind_param("i", $transaction);
+        $transaction_data->execute();
+        $transaction_data = $transaction_data->get_result()->fetch_assoc();
+
+        if ($check[$status]($transaction_data)) {
+            $actions[$status]($transaction);
+        }
     }
 }
 
-$transactions = $mysqli->prepare("SELECT transactions.id, transactions.bought_time, user.username, pizza.name AS pizza_name FROM transactions JOIN user ON user.id = transactions.receiver_id JOIN pizza ON pizza.id = transactions.pizza_id WHERE transactions.received_time IS NULL ORDER BY transactions.bought_time DESC");
+$transactions = $mysqli->prepare("SELECT transactions.id, transactions.bought_time, transactions.prepared_time , transactions.delivery_time , user.username, pizza.name AS pizza_name FROM transactions JOIN user ON user.id = transactions.receiver_id JOIN pizza ON pizza.id = transactions.pizza_id WHERE transactions.received_time IS NULL ORDER BY transactions.bought_time DESC");
 $transactions->execute();
 $transactions = $transactions->get_result()->fetch_all(MYSQLI_ASSOC);
 
@@ -33,10 +85,23 @@ foreach ($transactions as $transaction) {
             <p>Czas zamowienia: <?= $transaction["bought_time"] ?></p>
             <br>
 
-            <form action="?" method="post">
-                <input type="hidden" name="transaction_id" id="transaction_id" value="<?= $transaction["id"] ?>">
-                <button class="button is-primary">Oznacz jako dostarczone</button>
-            </form>
+            <?php
+
+            if ($check[$PREPARED]($transaction)) {
+                display_form($transaction, $PREPARED, "Oznacz jako w przygotowaniu");
+            } else if ($check[$DELIVERED]($transaction)) {
+                display_form($transaction, $DELIVERED, "Oznacz jako w dostawie");
+            } else if ($check[$RECEIVED]($transaction)) {
+                display_form($transaction, $RECEIVED, "Oznacz jako dostarczone");
+            } else {
+                ?>
+
+                <p>Nie mozesz nic zrobic w tej chwili :(</p>
+
+                <?php
+            }
+
+            ?>
         </div>
     </div>
 
