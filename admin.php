7229e4cb2d93680843cd76adc8e70711b88bf9e3
Xem0n <biedukiewicz87@gmail.com>
2023-03-09 11:13:16 +0100

    show subpages

--- admin.php
+++ admin.php
@@ -10,9 +10,70 @@ if (($_SESSION["user"] ?? null) == NULL || $_SESSION["user"]->role == "user") {
 
 $user = $_SESSION["user"];
 
+$pages = [
+    "users" => [
+        "file" => "users.php",
+        "name" => "Zarzadzaj uzytkownikami"
+    ],
+    "pizza" => [
+        "file" => "pizza.php",
+        "name" => "Zarzadzaj pizzami"
+    ],
+    "orders" => [
+        "file" => "orders.php",
+        "name" => "Potwierdz zamowienia"
+    ],
+    "messages" => [
+        "file" => "messages.php",
+        "name" => "Wiadomosci"
+    ]
+];
+
+$active_page = $_GET["page"] ?? "";
+
 ?>
 
-lolxd
+<div class="columns is-8">
+    <div class="column is-one-fifth box">
+        <aside class="menu">
+            <p class="menu-label">
+                Panel administratora
+            </p>
+
+            <ul class="menu-list">
+                <?php
+
+                foreach ($pages as $key => $page) {
+                    $is_active = $active_page == $key ? "is-active" : "";
+
+                    ?>
+
+                    <li><a class="<?= $is_active ?>" href="admin.php?page=<?= $key ?>"><?= $page["name"] ?></a></li>
+
+                    <?php
+                }
+
+                ?>
+            </ul>
+        </aside>
+    </div>
+
+    <div class="column box">
+        <?php
+
+        if (isset($pages[$active_page])) {
+            include_once "admin/" . $pages[$active_page]["file"];
+        } else {
+            ?>
+
+            <h5 class="title is-5">Wybierz podstrone</h5>
+
+            <?php
+        }
+
+        ?>
+    </div>
+</div>
 
 <?php
 
