5b52a95af6918631d99656bab2a284cbc9c4d0d7
Xem0n <biedukiewicz87@gmail.com>
2023-03-09 10:10:02 +0100

    fix minor bug

--- order.php
+++ order.php
@@ -10,7 +10,7 @@ if (!$_SESSION["user"] ?? null) {
 $pizza_id = $_POST["id"] ?? -1;
 $pizza_id = intval($pizza_id);
 
-$target_id = intval($_POST["user_id"]) ?? null;
+$target_id = intval($_POST["user_id"] ?? null);
 
 if (pizza_exist($pizza_id)) {
     pizza_order($pizza_id, $target_id);
