50e045d545f966980a57b7c219f394a650cbc49c
Xem0n <biedukiewicz87@gmail.com>
2023-03-30 09:34:49 +0200

    add font awesome

--- components/header.php
+++ components/header.php
@@ -13,6 +13,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lolxd</title>
+    <script src="https://kit.fontawesome.com/36c23ba1e4.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="static/style.css">
 </head>
